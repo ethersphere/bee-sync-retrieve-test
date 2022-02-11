5154589929ab54bd51e679a8f0c673a06aacbd79
sig <dan@1up.digital>
2022-02-11 09:06:31 +0000

    formatting

--- check.js
+++ check.js
@@ -1,44 +1,51 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
-const { Bee } = require('@ethersphere/bee-js')
+const { Bee } = require("@ethersphere/bee-js");
 
-const BEE_HOSTS = (process.env.BEE_HOSTS && process.env.BEE_HOSTS.split(',')) || ['http://localhost:1633']
-const bees = BEE_HOSTS.map(host => new Bee(host))
+const BEE_HOSTS = (process.env.BEE_HOSTS &&
+  process.env.BEE_HOSTS.split(",")) || ["http://localhost:1633"];
+const bees = BEE_HOSTS.map((host) => new Bee(host));
 
 async function tryRetrieveHash(bee, hash) {
-  const start = Date.now()
+  const start = Date.now();
   try {
-    await bee.downloadData(hash)
-    const end = Date.now()
+    await bee.downloadData(hash);
+    const end = Date.now();
 
-    console.log(`Bee ${bee.url} finished, elapsed time ${Math.ceil((end - start) / 1000)} secs, hash ${hash}`)
+    console.log(
+      `Bee ${bee.url} finished, elapsed time ${Math.ceil(
+        (end - start) / 1000
+      )} secs, hash ${hash}`
+    );
 
     return {
       bee: bee.url,
       hash,
-    }
+    };
   } catch (e) {
     return {
       bee: bee.url,
       hash: null,
-    }
+    };
   }
 }
 
 function error(e) {
-  throw e
+  throw e;
 }
 
 async function check() {
-  const hash = process.argv[2] || error('missing hash argument')
+  const hash = process.argv[2] || error("missing hash argument");
 
-  const responses = await Promise.all(bees.map(bee => tryRetrieveHash(bee, hash)))
-  const missing = responses.filter(response => response.hash === null).length
+  const responses = await Promise.all(
+    bees.map((bee) => tryRetrieveHash(bee, hash))
+  );
+  const missing = responses.filter((response) => response.hash === null).length;
 
-  console.log({ responses, missing })
+  console.log({ responses, missing });
 }
 
-check().catch(error => {
-  console.error({ error })
-  process.exit(1)
-})
+check().catch((error) => {
+  console.error({ error });
+  process.exit(1);
+});
